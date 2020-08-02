2ede1b093b2bf06c6e23b004a6ab95a7f6beed51
niranjanbhambi <niranjan.b@keydisruptors.com>
2020-08-03 03:58:17 +0530

    Login frontend UI modifications

--- resources/js/views/account/login.vue
+++ resources/js/views/account/login.vue
@@ -5,8 +5,8 @@
 export default {
   data() {
     return {
-      email: "admin@themesbrand.com",
-      password: "123456",
+      email: "",
+      password: "",
       tryingToLogIn: false,
       isAuthError: false
     };
@@ -30,53 +30,40 @@ export default {
 
 <template>
   <div class="row justify-content-center">
+    <div class="col-12 text-center mb-5">
+      <div>
+        <img src="/images/logo.svg" alt="Zenvision" class="img-fluid" />
+      </div>
+    </div>
     <div class="col-md-8 col-lg-6 col-xl-5">
       <div class="card overflow-hidden">
-        <div class="bg-soft-primary">
-          <div class="row">
-            <div class="col-7">
-              <div class="text-primary p-4">
-                <h5 class="text-primary">Welcome Back !</h5>
-                <p>Sign in to continue to Skote.</p>
-              </div>
-            </div>
-            <div class="col-5 align-self-end">
-              <img src="/images/profile-img.png" alt class="img-fluid" />
-            </div>
-          </div>
-        </div>
         <div class="card-body pt-0">
-          <div>
-            <a href="/">
-              <div class="avatar-md profile-user-wid mb-4">
-                <span class="avatar-title rounded-circle bg-light">
-                  <img src="/images/logo.svg" alt height="34" />
-                </span>
-              </div>
-            </a>
+          <div class="text-center">
+            <h4 class="text-white text-uppercase mb-4 mt-4">Login</h4>
           </div>
-
           <b-alert v-model="isAuthError" variant="danger" class="mt-3" dismissible>{{authError}}</b-alert>
 
           <b-form class="p-2" :action="submitUrl" method="POST">
             <slot />
-            <b-form-group id="input-group-1" label="Email" label-for="input-1">
+            <b-form-group id="input-group-1">
               <b-form-input
                 id="input-1"
                 name="email"
                 v-model="email"
-                type="text"
-                placeholder="Enter email"
+                type="email"
+                placeholder="Email"
+                required
               ></b-form-input>
             </b-form-group>
 
-            <b-form-group id="input-group-2" label="Password" label-for="input-2">
+            <b-form-group id="input-group-2">
               <b-form-input
                 id="input-2"
                 v-model="password"
                 name="password"
                 type="password"
-                placeholder="Enter password"
+                placeholder="Password"
+                required
               ></b-form-input>
             </b-form-group>
             <div class="custom-control custom-checkbox">
@@ -84,7 +71,7 @@ export default {
               <label class="custom-control-label" for="customControlInline">Remember me</label>
             </div>
             <div class="mt-3">
-              <b-button type="submit" variant="primary" class="btn-block">Log In</b-button>
+              <b-button type="submit" variant="primary" class="btn-block btn-success">Log In</b-button>
             </div>
             <div class="mt-4 text-center">
               <a href="/reset/password" class="text-muted">
