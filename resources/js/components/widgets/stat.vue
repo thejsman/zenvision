a0bfbad871f41d74ccc981f81863034ade207067
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2020-08-04 17:18:20 +0530

    Add Stats widget to show stats on homepage

---
+++ resources/js/components/widgets/stat.vue
@@ -0,0 +1,42 @@
+<script>
+/**
+ * Stat component -- specify the widget icon, title and value.
+ */
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    value: {
+      type: String,
+      default: ""
+    },
+    id: {
+      type: Number,
+      
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="card mini-stats-wid">
+    <div class="card-body">
+      <div class="media">
+        <div class="media-body">
+          <p class="text-muted font-weight-medium">{{title}}</p>
+          <h4 class="mb-0">{{value}}</h4>
+        </div>
+
+        <!-- <div class="mini-stat-icon avatar-sm align-self-center">
+          <span class="">
+            <i :class="`${icon} font-size-24`"></i>
+          </span>
+        </div> -->
+      </div>
+    </div>
+    <!-- end card-body -->
+  </div>
+  <!-- end card -->
+</template>
