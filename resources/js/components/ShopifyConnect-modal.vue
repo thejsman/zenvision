bcad35a7f66a2754dc2b4bcfcfc8895af63f68e9
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2020-08-12 21:31:04 +0530

    create shopify connect model

---
+++ resources/js/components/ShopifyConnect-modal.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="row">
+    <div class="m-4 d-flex flex-column">
+      <div class="font-weight-bold font-size-14 text-white">
+        <img src="/images/icons/shopify-icon.svg" alt height="21" class="mr-2" /> Connect to Shopify store
+      </div>
+      <p class="mt-4 mb-4">
+        Enter your existing
+        <strong>Shopify store URL</strong> below and you'll be redirected to Shopify to connect your account to Zenvision
+      </p>
+      <b-alert v-model="isAuthError" variant="danger" class="mt-3" dismissible>{{authError}}</b-alert>
+
+      <b-form class="p-2" :action="submitUrl" method="POST">
+        <slot />
+        <b-form-group id="input-group-1">
+          <b-form-input
+            id="input-1"
+            name="store_url"
+            v-model="store_url"
+            v-on:blur="onBlur"
+            v-on:paste="onPaste"
+            type="text"
+            placeholder="store-name.myshopify.com"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <div class="d-flex mt-4 justify-content-between align-items-center p-0">
+          <div>
+            <a
+              class="text-muted font-size-10"
+              href="#"
+              v-b-modal.how-to-connect-store
+            >How to connect my Shopify store?</a>
+          </div>
+          <div>
+            <b-button variant="primary" class="btn btn-cancel mr-2">Cancel</b-button>
+            <b-button type="submit" variant="primary" class="btn btn-success">Connect</b-button>
+          </div>
+        </div>
+      </b-form>
+    </div>
+    <b-modal id="how-to-connect-store" size="lg" centered hide-footer hide-header>
+      <HowToConnectStore />
+    </b-modal>
+  </div>
+</template>
+<script>
+import HowToConnectStore from "./HowToConnectStore-model";
+
+export default {
+  name: "ShopifyConnect",
+  components: { HowToConnectStore },
+  data() {
+    return {
+      store_url: "",
+      tryingToLogIn: false,
+      isAuthError: false,
+    };
+  },
+  props: {
+    submitUrl: {
+      type: String,
+    },
+    authError: {
+      type: String,
+      required: false,
+      default: () => null,
+    },
+  },
+  mounted() {
+    this.isAuthError = !!this.authError;
+  },
+  methods: {
+    onBlur: function (element) {
+      let store_url = this.store_url.trim();
+
+      if (store_url !== "") {
+        // remove https or http from url if exist
+        if (
+          store_url.indexOf("http://") == 0 ||
+          store_url.indexOf("https://") == 0
+        ) {
+          store_url = store_url.replace(/^https?\:\/\//i, "");
+        }
+
+        // remove anything after the .com
+        if (store_url.indexOf(".com") != -1) {
+          store_url = store_url.substring(0, store_url.indexOf(".com") + 4);
+        }
+
+        this.store_url = store_url;
+      }
+    },
+    onPaste: function (element) {
+      let clipboard_text = element.clipboardData.getData("Text");
+
+      if (clipboard_text !== "") {
+        this.store_url = clipboard_text;
+        $("#store_url").blur();
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.btn-cancel {
+  background-color: #32394f;
+  border: none;
+}
+</style>
