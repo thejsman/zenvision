38f1be8ac1bec8da0ccb4bae04e5ee0cacf194a3
imgio <gio1996@gmail.com>
2020-08-18 13:56:22 -0400

    Update app/Http/Controllers/UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,6 @@ use App\User;
 
 class UserController extends Controller
 {
-    //
     public function getStores(User $user) {
         // return User::find(auth()->user()->id)->stores;
         return $user->stores;
