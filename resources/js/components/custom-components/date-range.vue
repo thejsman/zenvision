13477374e8f72ab03aa861127e71c4530f8ea455
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2020-09-28 17:55:21 +0530

    add date-range component

--- resources/js/components/custom-components/date-range.vue
+++ resources/js/components/custom-components/date-range.vue
@@ -1,13 +1,52 @@
 <template>
-  <div class="ml-auto">
-    <ol class="text-center m-0">
-      <li class="list-unstyled">
-        Date Range:
-        <b-button variant="outline-light">01/01/2018 - 12/31/2018</b-button>
-      </li>
-    </ol>
+  <div class="ml-auto d-inline-flex">
+    <p class="mt-2 mr-2">Date Range:</p>
+    <date-range-picker
+      ref="picker"
+      :opens="opens"
+      :ranges="false"
+      v-model="dateRange"
+      :date-format="dateFormat"
+      @update="selectDate"
+    ></date-range-picker>
   </div>
 </template>
 <script>
-export default {};
+import DateRangePicker from "vue2-daterange-picker";
+import moment from "moment";
+import "vue2-daterange-picker/dist/vue2-daterange-picker.css";
+
+export default {
+  components: { DateRangePicker },
+
+  data() {
+    return {
+      dateRange: {
+        startDate: moment().add("month", -3),
+        endDate: moment(),
+      },
+      opens: "left",
+    };
+  },
+  methods: {
+    dateFormat(classes, date) {
+      if (!classes.disabled) {
+        classes.disabled = date.getTime() > new Date();
+      }
+      return classes;
+    },
+    selectDate() {
+      const { startDate, endDate } = this.dateRange;
+      console.log(
+        moment(startDate).format("DD/MM/YYYY"),
+        moment(endDate).format("DD/MM/YYYY")
+      );
+    },
+  },
+};
 </script>
+<style>
+.daterangepicker {
+  top: 40px;
+}
+</style>
