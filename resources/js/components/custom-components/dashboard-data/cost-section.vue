a34e11cdbe2ba47c174405f7e09ac077e5acbbe5
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2020-09-28 17:54:05 +0530

    add cost component

--- resources/js/components/custom-components/dashboard-data/cost-section.vue
+++ resources/js/components/custom-components/dashboard-data/cost-section.vue
@@ -3,7 +3,7 @@
     <div class="col-xl-12 mt-4">
       <h3>Cost</h3>
     </div>
-    <div v-for="cost of costData" :key="cost.id" class="col-md-4 p-2">
+    <div v-for="cost of data" :key="cost.id" class="col-md-4 p-2">
       <Stat :title="cost.title" :value="cost.value" />
     </div>
   </div>
@@ -12,10 +12,65 @@
 import Stat from "../../widgets/stat";
 export default {
   components: { Stat },
+  data() {
+    return {
+      data: [],
+    };
+  },
   props: {
     costData: {
       type: Array,
       default: () => [],
+    },
+    refundTotal: {
+      type: Number,
+      default: 0,
+    },
+  },
+  watch: {
+    costData(value, newValue) {
+      this.assignData(this.refundTotal, this.costData);
+    },
+  },
+  methods: {
+    assignData(refundTotal, orders) {
+      const cogs = _.sumBy(orders, (order) => parseFloat(order.cogs));
+      const discounts = _.sumBy(orders, (order) =>
+        parseFloat(order.total_discounts)
+      );
+
+      this.data = [
+        {
+          id: 1,
+          title: "Ad Spend",
+          value: "-",
+        },
+        {
+          id: 2,
+          title: "COGS",
+          value: `$` + cogs,
+        },
+        {
+          id: 3,
+          title: "Merchant Fees",
+          value: "-",
+        },
+        {
+          id: 4,
+          title: "Refunds",
+          value: `$` + refundTotal,
+        },
+        {
+          id: 5,
+          title: "Chargebacks",
+          value: "-",
+        },
+        {
+          id: 6,
+          title: "Discounts",
+          value: `$` + discounts,
+        },
+      ];
     },
   },
 };
