c9f3ade52458f5c9960f846a9eca7c29f34f4dd1
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2020-12-14 22:21:30 +0530

    Add util and constants file

---
+++ resources/js/constants.js
@@ -0,0 +1,13 @@
+// Revenue Section Constants
+export const NUMBER_OF_ORDERS = "Number of Orders";
+export const ORDER_REVENUE = "Order Revenue";
+export const SHIPPING_REVENUE = "Shipping Revenue";
+export const TAXES_REVENUE = "Taxes";
+
+// Key Performance Section Constants
+export const CONVERSION_RATE = "Conversion Rate";
+export const ABANDONED_CART = "Abandoned Cart";
+export const AVERAGE_ORDER_VALUE = "Avg Order Value";
+export const AVERAGE_UNITS_PER_ORDER = "Avg Units Per Order";
+export const AVERAGE_PROFIT_PER_ORDER = "Avg Profit Per Orders";
+export const US_ORDERS_PERCENTAGE = "% US Orders";
