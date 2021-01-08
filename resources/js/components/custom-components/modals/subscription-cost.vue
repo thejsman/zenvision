a7e9f5daf6ca5f91e9e1a68d5b7e6909fb85ea56
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2021-01-08 14:33:23 +0530

    Subscription cost modal

---
+++ resources/js/components/custom-components/modals/subscription-cost.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-row>
+    <h1>Subscription cost</h1>
+    <b-col cols="10" offset="1">
+      <div class="font-weight-bold font-size-18 text-white mt-4">
+        Subscription Costs
+      </div>
+      <div class="d-flex justify-content-between align-items-center">
+        <div>
+          <p class="mt-4 mb-4 text-white">
+            This represents recurring subscription expenses.
+          </p>
+        </div>
+        <b-button
+          variant="primary"
+          class="btn btn-success"
+          @click="handleAddSubscriptionCost"
+          >Add</b-button
+        >
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
