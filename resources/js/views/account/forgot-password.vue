278545004b853806ab8c9e406762176100a0a658
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2021-02-18 13:05:34 +0530

    ZV-103:Forgot your password? bug-fix

--- resources/js/views/account/forgot-password.vue
+++ resources/js/views/account/forgot-password.vue
@@ -47,8 +47,20 @@ export default {
             <h4 class="text-white text-uppercase mb-4 mt-4">reset password</h4>
           </div>
           <div class="p-2">
-            <b-alert v-model="isResetError" class="mb-4" variant="danger" dismissible>{{error}}</b-alert>
-            <b-alert v-model="tryingToReset" class="mb-4" variant="success" dismissible>{{status}}</b-alert>
+            <b-alert
+              v-model="isResetError"
+              class="mb-4"
+              variant="danger"
+              dismissible
+              >{{ error }}</b-alert
+            >
+            <b-alert
+              v-model="tryingToReset"
+              class="mb-4"
+              variant="success"
+              dismissible
+              >{{ status }}</b-alert
+            >
             <form :action="submitUrl" method="POST">
               <slot />
               <div class="form-group">
@@ -64,7 +76,12 @@ export default {
               </div>
               <div class="form-group row mb-3 pt-2">
                 <div class="col-12 text-right">
-                  <button class="btn btn-primary btn-success btn-block" type="submit">Reset</button>
+                  <button
+                    class="btn btn-primary btn-success btn-block"
+                    type="submit"
+                  >
+                    Reset
+                  </button>
                 </div>
               </div>
             </form>
@@ -77,7 +94,9 @@ export default {
       <div class="mt-5 text-center">
         <p>
           Remember It ?
-          <a href="/login" class="font-weight-medium text-primary">Log in here</a>
+          <a href="/login" class="font-weight-medium text-primary"
+            >Log in here</a
+          >
         </p>
       </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-@php
-$layout = isset($_COOKIE['layout']) ? $_COOKIE['layout'] : "dark";
-$css_file = "";
-switch ($layout) {
-case 'dark':
-$css_file = "css/app-dark.css";
-break;
-case 'rtl':
-$css_file = "css/app-rtl.css";
-break;
-default:
-$css_file = "css/app.css";
-break;
-}
-@endphp
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,15 +8,14 @@ break;
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="icon" href="{{ url('favicon.ico') }}">
     <title>
-        @yield('title', config('app.name', 'Skote'))
+        @yield('title', config('app.name', 'Zenvision'))
     </title>
     <meta
         content="Zenvision"
         name="description" />
     <script src="/js/app.js" defer></script>
-    <link href="css/app-dark.css" rel="stylesheet" id="layout-css">
+    <link href="/css/app-dark.css" rel="stylesheet" id="layout-css">
 </head>
-
 <body>
     <noscript>
         <strong>We're sorry but Zenvision doesn't work properly without JavaScript enabled. Please enable it to
