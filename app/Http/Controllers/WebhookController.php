17d0513be2fbea95e488932c73b6183b94e02dfc
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2021-03-02 16:53:28 +0530

    orde delete issue

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -100,7 +100,7 @@ class WebhookController extends Controller
 
     public function ordersDelete(Request $request)
     {
-        $order = ShopifyOrder::where('order_id', '=', $request->id);
+        $order = ShopifyOrder::where('order_id', $request->id)->first();
         $order->is_deleted = true;
         $order->save();
     }
