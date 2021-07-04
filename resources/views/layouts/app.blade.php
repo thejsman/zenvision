eaffaf866085e868e031b052b915d6a94d2625bf
Lucas <Lucas>
2021-07-04 14:10:08 +0200

    Updated favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <link rel="icon" href="{{ url('favicon.ico') }}">
+    <link rel="icon" href="{{ url('favicon.png') }}">
     <title>
         @yield('title', config('app.name', 'Zenvision'))
     </title>
