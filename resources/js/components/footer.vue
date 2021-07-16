d89c77f815596f24afe8f7dca87fb42e92246b49
Niranjan Bhambi <niranjan.bhambi@hotmail.com>
2021-07-16 22:50:42 +0530

    footer tnc and privacy policy links

--- resources/js/components/footer.vue
+++ resources/js/components/footer.vue
@@ -1,10 +1,36 @@
 <template>
-  <footer class="footer">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-6">{{ new Date().getFullYear() }} © Zenvision.</div>
-      </div>
-    </div>
-  </footer>
-  <!-- end footer -->
+    <footer class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6">
+                    {{ new Date().getFullYear() }} © Zenvision.
+                </div>
+                <div class="col-sm-6">
+                    <div class="d-flex justify-content-end text-white">
+                        <a
+                            href="https://www.zenvision.io/terms-and-conditions/"
+                            target="_blank"
+                            class="text-white footer-link"
+                            >Terms & Conditions</a
+                        >
+                        <a
+                            href="https://www.zenvision.io/privacy-policy/"
+                            target="_blank"
+                            class="ml-3 text-white footer-link"
+                            >Privacy Policy</a
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- end footer -->
 </template>
+<style>
+.footer-link {
+    opacity: 0.5;
+}
+.footer-link:hover {
+    opacity: 1;
+}
+</style>
