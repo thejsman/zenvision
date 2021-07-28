068885a15197dda2e519d1cdc916ca8328f9bf13
Niranjan Bhambi <niranjan.bhambi@hotmail.com>
2021-07-28 21:03:28 +0530

    CustomerRedactRequest response

--- app/Http/Controllers/GdprWebhook.php
+++ app/Http/Controllers/GdprWebhook.php
@@ -23,4 +23,23 @@ class GdprWebhook extends Controller
             return response()->json(['status' => 'success', 'data_request' => $request->data_request, 'message' => 'Data not found', 'data' => []], 200);
         }
     }
+
+    public function CustomerRedactRequest(Request $request)
+    {
+        $shop_domain = $request->shop_domain;
+        $shop_details = ShopifyStore::where('store_url', $shop_domain)->first();
+        if ($shop_details) {
+            $orders_redacted = [];
+            foreach ($request->orders_to_redact as $req_order) {
+                $response = ShopifyOrder::where('order_number', $req_order)->where('store_id', $shop_details->id)->delete();
+
+                if ($response) {
+                    array_push($orders_redacted, $req_order);
+                }
+            }
+            return response()->json(['status' => 'success', 'orders_redacted' => $orders_redacted], 200);
+        } else {
+            return response()->json(['status' => 'success',  'message' => 'Data not found',], 200);
+        }
+    }
 }
