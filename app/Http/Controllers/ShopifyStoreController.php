a354cd00d95dbd46c8657001942c04d4da8c161a
Niranjan Bhambi <niranjan.bhambi@hotmail.com>
2021-08-10 07:59:24 +0530

    check - 11

--- app/Http/Controllers/ShopifyStoreController.php
+++ app/Http/Controllers/ShopifyStoreController.php
@@ -94,7 +94,7 @@ class ShopifyStoreController extends Controller
 
     public function getResponse(Request $request)
     {
-
+        dd($request);
         // response code from shopify
         $response_code = $request->input('code');
         // shopify store domain
